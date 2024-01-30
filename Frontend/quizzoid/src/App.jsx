e1cdf84a46bd3f4244d30e20f4c2d4aae0435be2
0x46641766F7572 <adeoyefav@gmail.com>
2024-01-30 03:48:09 +0100

    update

--- Frontend/quizzoid/src/App.jsx
+++ Frontend/quizzoid/src/App.jsx
@@ -2,6 +2,7 @@ import{
   createBrowserRouter, 
   createRoutesFromElements, 
   Route,
+  Navigate,
   RouterProvider
   } from 'react-router-dom';
 import Login from '../components/Login';
@@ -9,16 +10,25 @@ import Register from '../components/Register';
 import RootLayout from '../layout/Rootlayout';
 import Onboarding from '../pages/onboarding';
 
-
+import { useSelector} from "react-redux";
+import { Children } from 'react';
 
 export default function App() {
+  const {userInfo} = useSelector((state)=> state.auth);
+
+  const ProtectedRoute = ({children})=> {
+    if(!userInfo){
+    return <Navigate to="/login"/>
+  }
+  return children
+}
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path="/" element={<RootLayout/>}>
       <Route index={true} path="/" element={<Login/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Register/>}/>
-      <Route path='/onboarding' element={<Onboarding/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="register" element={<Register/>}/>
+      <Route path='onboarding' element={<ProtectedRoute><Onboarding/></ProtectedRoute>}/>
     </Route>
     )
    )
